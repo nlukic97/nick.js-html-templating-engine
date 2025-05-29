c91a52dbfd57dd8979804befc02db6340867da55
nlukic97 <nlukic97@gmail.com>
2025-05-29 14:03:49 +0200

    Importing view returning function from lib folder

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { createServer } = require('node:http');
 
-const {view} = require('./src/renderContent')
+const {view} = require('./lib/renderer')
 
 const app = express();
 const server = createServer(app);
@@ -13,8 +13,8 @@ function test(req,res,next){
 
 
 // Routes
-app.get('/', test, view('./src/index.html'));
-app.get('/about', test, view("./src/about.html"));
+app.get('/', test, view('index.html'));
+app.get('/about', test, view("about.html"));
 
 
 server.listen(3000, () => console.log('Server running at http://localhost:3000'));
